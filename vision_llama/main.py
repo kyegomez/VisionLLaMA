ce5cca7178542a32a64672772997d56f95057837
Kye <kye@apacmediasolutions.com>
2024-03-04 18:42:44 -0800

    [FEAT][Rotary Embedding + Patch Embedding]

--- vision_llama/main.py
+++ vision_llama/main.py
@@ -1,10 +1,20 @@
+import torch
 from torch import nn, Tensor
 from zeta.nn import (
     SwiGLUStacked,
     MultiQueryAttention,
     img_to_text,
-    LocalAttention,
+    
 )
+from local_attention import LocalAttention
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+from math import sqrt, pi
+
+
+# Pair
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
 
 
 class AS2DRoPE(nn.Module):
@@ -19,6 +29,41 @@ class AS2DRoPE(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         pass
+
+
+class AxialRotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_freq=10):
+        super().__init__()
+        self.dim = dim
+        scales = torch.linspace(1.0, max_freq / 2, self.dim // 4)
+        self.register_buffer("scales", scales)
+
+    def forward(self, x):
+        device, dtype, n = x.device, x.dtype, int(sqrt(x.shape[-2]))
+
+        seq = torch.linspace(-1.0, 1.0, steps=n, device=device)
+        seq = seq.unsqueeze(-1)
+
+        scales = self.scales[
+            (*((None,) * (len(seq.shape) - 1)), Ellipsis)
+        ]
+        scales = scales.to(x)
+
+        seq = seq * scales * pi
+
+        x_sinu = repeat(seq, "i d -> i j d", j=n)
+        y_sinu = repeat(seq, "j d -> i j d", i=n)
+
+        sin = torch.cat((x_sinu.sin(), y_sinu.sin()), dim=-1)
+        cos = torch.cat((x_sinu.cos(), y_sinu.cos()), dim=-1)
+
+        sin, cos = map(
+            lambda t: rearrange(t, "i j d -> (i j) d"), (sin, cos)
+        )
+        sin, cos = map(
+            lambda t: repeat(t, "n d -> () n (d j)", j=2), (sin, cos)
+        )
+        return sin, cos
 
 
 class VisionLlamaBlock(nn.Module):
@@ -52,11 +97,13 @@ class VisionLlamaBlock(nn.Module):
         self,
         dim: int,
         depth: int,
-        channels: int,
-        heads: int,
+        channels: int = 3,
+        heads: int = 12,
         dim_head: int = 64,
         mlp_mult: int = 4,
         dropout: float = 0.0,
+        image_size: int = 224,
+        patch_size: int = 16,
         *args,
         **kwargs,
     ):
@@ -68,6 +115,10 @@ class VisionLlamaBlock(nn.Module):
         self.dim_head = dim_head
         self.mlp_mult = mlp_mult
         self.dropout = dropout
+        
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+        patch_dim = channels * patch_height * patch_width
 
         # Norm
         self.norm = nn.LayerNorm(dim)
@@ -86,17 +137,40 @@ class VisionLlamaBlock(nn.Module):
             # qk_ln=True,
         )
 
+        # AxialRotaryEmbedding
+        self.axial_rotary = AxialRotaryEmbedding(dim)
+        
+        # To patch embeddings
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange(
+                "b c (h p1) (w p2) -> b (h w) (p1 p2 c)",
+                p1 = patch_height,
+                p2 = patch_width
+            ),
+            nn.Linear(patch_dim, dim),
+        )
+
     def forward(self, x: Tensor) -> Tensor:
         b, c, h, w = x.shape
-
-        x = img_to_text(x, self.channels, self.dim)
+        
+        # Patch Embedding
+        x = self.to_patch_embedding(x)
+        print(x.shape)
+        
+        # Reshape to text
+        # x = img_to_text(x, self.channels, self.dim)
+        print(x.shape)
+        
         skip_1 = x
         print(x.shape)
 
         # Norm
         x = self.norm(x)
+        print(x.shape)
 
         # as2d rope
+        x, _ = self.axial_rotary(x)
+        print(x.shape)
 
         # Attn
         x, _, _ = self.attn(x)
@@ -154,6 +228,8 @@ class VisionLlamaPyramidBlock(nn.Module):
         mlp_mult: int = 4,
         dropout: float = 0.0,
         local_window_size: int = 512,
+        image_size: int = 224,
+        patch_size: int = 16,
         *args,
         **kwargs,
     ):
@@ -165,6 +241,12 @@ class VisionLlamaPyramidBlock(nn.Module):
         self.dim_head = dim_head
         self.mlp_mult = mlp_mult
         self.dropout = dropout
+        
+        # Image height
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+        patch_dim = channels * patch_height * patch_width
+        
 
         # Layernorm
         self.norm = nn.LayerNorm(dim)
@@ -182,6 +264,9 @@ class VisionLlamaPyramidBlock(nn.Module):
             **kwargs,
             # qk_ln=True,
         )
+        
+        # AxialRotaryEmbedding
+        self.rotary_embed = AxialRotaryEmbedding(dim)
 
         # Local Attention
         self.local_attn = LocalAttention(
@@ -191,14 +276,27 @@ class VisionLlamaPyramidBlock(nn.Module):
             autopad=True,
             shared_qk=True,
             use_rotary_pos_emb=True,
-            use_xpos=True,
+            # use_xpos=True,
+        )
+        
+        # To patch embeddings
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange(
+                "b c (h p1) (w p2) -> b (h w) (p1 p2 c)",
+                p1 = patch_height,
+                p2 = patch_width
+            ),
+            nn.Linear(patch_dim, dim),
         )
 
     def forward(self, x: Tensor) -> Tensor:
         b, c, h, w = x.shape
+        
+        # Patch Embedding
+        x = self.to_patch_embedding(x)
 
         # Convert image to text
-        x = img_to_text(x, self.channels, self.dim)
+        # x = img_to_text(x, self.channels, self.dim)
 
         # Skip connection
         skip_1 = x
@@ -207,10 +305,10 @@ class VisionLlamaPyramidBlock(nn.Module):
         normed = self.norm(x)
 
         # as2drope
-        # pass
+        normed, _ = self.rotary_embed(normed)
 
         # Local Attention with skip connect
-        x = self.local_attn(normed) + skip_1
+        x = self.local_attn(normed, normed, normed) + skip_1
 
         # 2nd skip connection
         skip_2 = x
@@ -225,15 +323,21 @@ class VisionLlamaPyramidBlock(nn.Module):
         x = self.norm(x)
 
         # as2drope
+        x, _ = self.rotary_embed(x)
 
         # Global Attention
+        x, _, _ = self.attn(x) 
+        x = x + skip_1
 
         # residual connection
-
+        skip_3 = x
+        
         # Norm
+        x = self.norm(x)
 
         # SWIGLU
+        x = self.act(x) + skip_3
 
         # Skip connection
 
-        # return
+        return x
